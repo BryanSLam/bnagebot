023a8ae3bf99b4abb6f6ac7c2a3000b08bda8d2e
BryanSLam <bryan.s.lam@gmail.com>
2016-03-01 00:39:49 -0800

    added listener examples and tested on channels

--- config/config.js
+++ config/config.js
@@ -4,7 +4,7 @@ config.server = 'irc.twitch.tv';
 config.port = '6667';
 config.username = 'bnagebot';
 config.oauth = 'oauth:ckwo02s3fz0ffasdxzl6rpkd84u02s';
-config.channels = ['#bnage'];
+config.channels = ['#bnage', '#freakinjolly'];
 
 
 module.exports = config;
