f547c52cac99e92c5d71fac1505db6d46290150c
BryanSLam <bryan.s.lam@gmail.com>
2016-03-09 01:26:29 -0800

    saves properly but doesnt parse readfiel correctly yet

--- bot.js
+++ bot.js
@@ -3,57 +3,60 @@ var irc = require('irc');
 var helper = require('./helper.js');
 var fs = require('fs');
 
-var commands = {};
-var writeStream = null;
 //Setup returns a hash with the tools necessary to run the bot.
-helper.setUp(function(data) {
-//    writeStream = fs.createWriteStream('./data/custom_command.txt');
-    console.log("callback done");
-});
-
-var bot = new irc.Client(config.server, config.username, {
-  channels: config.channels,
-  port: config.port,
-  userName: config.username,
-  password: config.oauth
-});
-
-
-bot.addListener('message', function (from, to, message) {
-    console.log(from + ' => to ' + message);
+helper.setUp(function (data) {
+    console.log(JSON.stringify(data));
+    var commands = data;
     
-    //COMMANDS
-    if(message.charAt(0) === '!') {
-        var com = message.split(' ');
-        if(message.substring(0,8) === '!command') {
-            if(com[1] === 'save') {
-                var newCommand = helper.newCommand(message);
-                if (newCommand[0] == '!command') {
-                    bot.say(to, 'Fuck you nice try bitchass');
+    var bot = new irc.Client(config.server, config.username, {
+        channels: config.channels,
+        port: config.port,
+        userName: config.username,
+        password: config.oauth
+    });
+
+    bot.addListener('message', function (from, to, message) {
+        console.log(from + ' => to ' + message);
+        //COMMANDS
+        if (message.charAt(0) === '!') {
+            var com = message.split(' ');
+            if (message.substring(0, 8) === '!command') {
+                if (com[1] === 'save') {
+                    var newCommand = helper.newCommand(message);
+                    if (newCommand[0] == '!command') {
+                        bot.say(to, 'Fuck you nice try bitchass');
+                    }
+                    if (com[2].charAt(0) != '!') {
+                        bot.say(to, 'Invalid save, command must start with !');
+                    } else {
+                        //TODO
+                        //Error check, if command exists already then overwrite
+                        commands[newCommand[0]] = newCommand[1];
+                        var text = newCommand[0] + ':' + newCommand[1] + ' ';
+                        fs.appendFile('./data/custom_commands.txt', text, function (err) {
+                            if (err) throw err;
+                            bot.say(to, 'Command ' + newCommand[0] + ' saved!');
+                        });
+                    }
                 }
-                if(com[2].charAt(0) != '!') {
-                    bot.say(to, 'Invalid save, command must start with !');
-                } else {
-                    //TODO
-                    //Error check, if command exists already then overwrite3
-                    
-                    commands[newCommand[0]] = newCommand[1];
-                    var text = newCommand[0] + ':' + newCommand[1] + ' ';
-                    fs.appendFile('./data/custom_commands.txt', text, function(err) {
-                        if (err) throw err;
-                        bot.say(to, 'Command ' + newCommand[0] + ' saved!');
-                    });
-                }
+                //If its not one of the preset commands then its a custom command
+            } else {
+                bot.say(to, commands[com[0]]);
             }
-        //If its not one of the preset commands then its a custom command
-        } else {
-            bot.say(to, commands[com[0]]);
         }
-    }
-    
-    if (from == 'freakinjolly') {
-        bot.say(to, 'STFU DIKO');
-    } else {
-        bot.say(to, 'You da best');
-    }
+
+        if (from == 'freakinjolly') {
+            bot.say(to, 'STFU DIKO');
+        } else {
+            bot.say(to, 'You da best');
+        }
+    });
 });
+
+/*
+bnage: !diko5
+bnagebot: diko (MISSING SUCKS)
+bnagebot: You da best
+
+FIX THIS, ISNT DEALING WITH WHITE SPACE CORRECTLY
+*/
--- helper.js
+++ helper.js
@@ -6,7 +6,9 @@ exports.newCommand = function (message) {
     var output = [];
     var arr = message.split(' ');
     output[0] = arr[2];
-    output[1] = message.substring(14 + (arr[2].length));
+    output[1] = message.substring(15 + (arr[2].length));
+    console.log(output[0] + "fff");
+    console.log(output[1] + "fff");
     return output;
 };
 
@@ -14,7 +16,7 @@ exports.newCommand = function (message) {
  * Sets up bot preliminary data
  */
 exports.setUp = function (callback) {
-    var data = {};
+    var hash = {};
     //Checks for the existence of the custom commands file, if not create one
     fs.access('./data/custom_commands.txt', fs.R_OK | fs.W_OK, (err) => {
         console.log(err ? 'no access!' : 'can read/write');
@@ -22,16 +24,21 @@ exports.setUp = function (callback) {
             fs.writeFile('./data/custom_commands.txt', '', function (err) {
                 if (err) throw err;
                 console.log('new custom commands file created.');
-                data.commands = {};
-                callback(data);
+                callback(hash);
             });
         } else {
             //Read the file and return the data stored as a hash
-            fs.readFile('./data/custom_commands.txt', function (err, data) {
+            fs.readFile('./data/custom_commands.txt', 'utf-8', function (err, data) {
                 if (err) throw err;
-                console.log(data);
                 //Parse the read file
-                callback(data);
+                var i = 0;
+                var arr = data.split(' ');
+                var temp = [];
+                for (i = 0; i< arr.length; i++) {
+                    temp = arr[i].split(':');
+                    hash[temp[0]] = temp[1];
+                }
+                callback(hash);
             });
         }
     });
