f7e7f11974947ee84a4351b6859aad750e406d50
BryanSLam <bryan.s.lam@gmail.com>
2016-03-14 22:43:52 -0700

    changed error handling

--- bot.js
+++ bot.js
@@ -2,41 +2,30 @@ var config = require('./config/config.js');
 var irc = require('irc');
 var helper = require('./helper.js');
 var fs = require('fs');
+var path = './data/custom_commands.txt';
 
 //Setup returns a hash with the tools necessary to run the bot.
 helper.setUp(function (data) {
-    
+
     console.log(JSON.stringify(data));
     var commands = data;
-    
-    //On exit, save everything currently in commands into the file
-    
-    //CHANGE THE ERROR HANDLING, THIS ISNT GOOD FOR A PROGRAM YOU WANT TO SUSTAIN 
-    //FOREVER, we don't want the bot to continously exit just to commit data
-    helper.cleanUp(function() {
+
+    helper.cleanUp(function () {
         var commandsText = '';
         console.log("CLEANUP HAPPENING");
-        for (var key in commands) {
-            console.log('looping');
-            if(commands.hasOwnProperty(key)) {
-                commandsText = commandsText.concat(key + ':' + commands[key] + ';');
-            }
-        }
-        console.log(commandsText);
-//        fs.writeFileSync('./data/custom_commands.txt', 'commandsText', 'utf-8', function(err) {
-//            if (err) throw err;
-//            console.log('write');
-//        });
     });
-    
+
     var bot = new irc.Client(config.server, config.username, {
         channels: config.channels,
         port: config.port,
         userName: config.username,
         password: config.oauth
     });
-    
+
     bot.addListener('message', function (from, to, message) {
+        //ADD COMMAND TO POLL 1'S AND 2'S
+
+
         console.log(from + ' => to ' + message);
         //COMMANDS
         if (message.charAt(0) === '!') {
@@ -50,12 +39,26 @@ helper.setUp(function (data) {
                     if (com[2].charAt(0) != '!') {
                         bot.say(to, 'Invalid save, command must start with !');
                     } else {
-                        commands[newCommand[0]] = newCommand[1];
-//                        var text = newCommand[0] + ':' + newCommand[1] + ';';
-//                        fs.appendFile('./data/custom_commands.txt', text, function (err) {
-//                            if (err) throw err;
-//                            bot.say(to, 'Command ' + newCommand[0] + ' saved!');
-//                        });
+                        var text = newCommand[0] + ':' + newCommand[1] + ';';
+                        //Duplicate handling
+                        if (newCommand[0] in commands) {
+                            fs.readFile(path, 'utf8', function (err, data) {
+                                if (err) throw err;
+                                console.log('duplicate read');
+                                var commandReplace = newCommand[0] + ':' + commands[newCommand[0]] + ';';
+                                var result = data.replace(commandReplace, text);
+                                fs.writeFile(path, result, 'utf8', function (err) {
+                                    if (err) throw err;
+                                    commands[newCommand[0]] = newCommand[1];
+                                });
+                            });
+                        } else {
+                            fs.appendFile(path, text, function (err) {
+                                if (err) throw err;
+                                bot.say(to, 'Command ' + newCommand[0] + ' saved!');
+                                commands[newCommand[0]] = newCommand[1];
+                            });
+                        }
                     }
                 }
                 //If its not one of the preset commands then its a custom command
